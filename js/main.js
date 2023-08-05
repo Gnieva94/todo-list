465e505c60cccace6c576b850d5f958e9537546b
Gnieva94 <gus_94@hotmail.com.ar>
2023-08-04 23:59:22 -0300

    Finish popup and styles

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const form_nombre = document.getElementById('form_nombre');
 const container_task = document.getElementById('container_task');
 const list_container = document.getElementById('list_container');
-
+const fondo_popup = document.getElementById('fondo_popup');
+const popup = document.getElementById('popup');
 
 form_nombre.addEventListener('submit', function(e){
     e.preventDefault();
-    const input_nombre = document.getElementById('input_nombre').value;
-    console.log(typeof input_nombre);
-    if(input_nombre != ""){
+    if(input_nombre.value != ""){
+        const input_nombre = document.getElementById('input_nombre');
         const nombre_principal = document.getElementById('nombre_principal');
-        nombre_principal.innerHTML = `Hola! ${input_nombre}`;
+        nombre_principal.innerHTML = `Hola! <span id="nombre">${input_nombre.value}</span>`;
         form_nombre.reset();
         form_nombre.classList.add('d-none');
         const container_principal = document.getElementById('container_principal');
@@ -32,11 +32,25 @@ container_task.addEventListener('submit', function(e){
     }
 });
 
-
 list_container.addEventListener('click', function(e){
     if(e.target.tagName === 'INPUT'){
-        e.target.nextElementSibling.classList.toggle('decoration_p');
+        const nextSibling = e.target.nextElementSibling;
+        nextSibling.classList.toggle('decoration_p');
+        if(e.target.checked){
+            fondo_popup.classList.toggle('d-none');
+            popup.classList.toggle('d-none');
+            const nombre = document.getElementById('nombre');
+            popup.innerHTML = `<h3>Muy bien, ${nombre.innerHTML}!</h3>
+            <p>Has completado la tarea "${nextSibling.innerHTML}"!</p>
+            `
+        }
     }
+});
+
+fondo_popup.addEventListener('click', function(e){
+    e.preventDefault();
+    fondo_popup.classList.toggle('d-none');
+    popup.classList.toggle('d-none');
 });
 
 
